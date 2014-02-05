4fe3c8ccddabb7207a46672ac2d7212ba2304e64
sin <sin@2f30.org>
2014-02-05 18:29:46 +0000

    Rename FIFO path for sinit

--- stuff/sinit.c
+++ stuff/sinit.c
@@ -30,7 +30,7 @@ static void cmdreboot(const Arg *);
 static void dispatchcmd(int);
 static int spawn(const char *, char *const []);
 
-static const char *fifopath = "/var/run/init.fifo";
+static const char *fifopath = "/var/run/sinit.fifo";
 static Command commands[] = {
 	{ "poweroff",   cmdpoweroff,   {0} },
 	{ "reboot",     cmdreboot,     {0} },
