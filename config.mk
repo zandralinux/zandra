1c117e6f7498464e491201ab6a843bc8318d1bb8
sin <sin@2f30.org>
2014-02-28 18:14:53 +0000

    Comment out optcflags and optldflags
    These mostly work but not for all packages.  ncurses and/or libevent
    seem to have trouble with them.

--- config.mk
+++ config.mk
@@ -8,10 +8,11 @@ version = 0.0
 mirror = http://dl.2f30.org/morpheus-pkgs/${arch}/${version}
 CC = ${arch}-linux-musl-gcc
 
+# these don't work for all packages yet...
 optldflags = -s -Wl,--gc-sections -Wl,-z,relro,-z,now
 optcflags = -fdata-sections -ffunction-sections -Os -g0 -fno-unwind-tables -fno-asynchronous-unwind-tables -Wa,--noexecstack
 
-CFLAGS = -I${libcroot}/include -static ${optcflags}
-LDFLAGS = -L${libcroot}/lib -static ${optldflags}
+CFLAGS = -I${libcroot}/include -static # ${optcflags}
+LDFLAGS = -L${libcroot}/lib -static # ${optldflags}
 
 PATH = ${root}/opt/cross/${arch}-linux-musl/bin:${PATH}
