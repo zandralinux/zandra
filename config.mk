6f8a42e27464c6933b9fe83f324cc1c1d6d9eca0
sin <sin@2f30.org>
2014-02-28 13:41:26 +0000

    Start using $optldflags and $optcflags

--- config.mk
+++ config.mk
@@ -8,5 +8,7 @@ version = 0.0
 mirror = http://dl.2f30.org/morpheus-pkgs/${arch}/${version}
 CC = ${arch}-linux-musl-gcc
 
-PATH = ${root}/opt/cross/${arch}-linux-musl/bin:${PATH}
+optldflags = "-s -Wl,--gc-sections -Wl,-z,relro,-z,now"
+optcflags = "-fdata-sections -ffunction-sections -Os -g0 -fno-unwind-tables -fno-asynchronous-unwind-tables -Wa,--noexecstack"
 
+PATH = ${root}/opt/cross/${arch}-linux-musl/bin:${PATH}
