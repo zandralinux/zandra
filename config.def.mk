56acf67326c232d98e4433b2de06dc0f983ea3cc
Daniel Bainton <dpb@driftaway.org>
2014-03-26 21:19:02 +0000

    Complete rewrite of the build system

--- config.def.mk
+++ config.def.mk
@@ -1,20 +1,29 @@
-#arch = i486
-arch = x86_64
-root = ${top}/root
-nprocs = 4   # number of processes for building separate packages
-NPROC = 4    # number of processes for mk concurrency
-
-libcroot = ${root}/opt/cross/${arch}-linux-musl/${arch}-linux-musl
+arch = i486
 version = 0.0
 mirror = http://dl.2f30.org/morpheus-pkgs/${arch}/${version}
-CC = ${arch}-linux-musl-gcc
+
+TOOL_BASE = ${arch}-musl-linux-
+CC = ${TOOL_BASE}gcc
+LD = $CC
+AR = ${TOOL_BASE}ar
+RANLIB = ${TOOL_BASE}ranlib
+STRIP = ${TOOL_BASE}strip
+#STRIP = true
+SHELL = /bin/sh
+
+PREFIX =
+BINDIR = ${PREFIX}/bin
+LIBDIR = ${PREFIX}/lib
+ETCDIR = ${PREFIX}/etc
+DFLDIR = ${ETCDIR}/default
+MANDIR = ${PREFIX}/share/man
+
+INSTALL = install
 
 # these don't work for all packages yet...
 optldflags = -s -Wl,--gc-sections -Wl,-z,relro,-z,now
 optcflags = -fdata-sections -ffunction-sections -Os -g0 -fno-unwind-tables -fno-asynchronous-unwind-tables -Wa,--noexecstack
 
 CPPFLAGS = -D_BSD_SOURCE -D_GNU_SOURCE
-CFLAGS = -I${libcroot}/include ${CPPFLAGS} # ${optcflags}
-LDFLAGS = -L${libcroot}/lib -static ${optldflags}
-
-PATH = ${root}/opt/cross/${arch}-linux-musl/bin:${PATH}
+CFLAGS = ${CPPFLAGS} # ${optcflags}
+LDFLAGS = -static ${optldflags}
--- mkfile
+++ mkfile
@@ -1,33 +1,7 @@
-top = `{pwd}
 `{ test -e config.mk || cp config.def.mk config.mk }
-<config.mk
 
-allpkgs = `{ls -d pkgs/*}
-pkgs = ${allpkgs:pkgs/%=%}
+<$mkbuild/mk.3rdparty
 
-help:VQ:
-	printf 'Usage: mk pkg...\n'
-	printf '\nTo build all the targets in pkgs/, run `mk world´\n'
-
-all world:VQ: $pkgs
-
-clean:V:
-	rm -f .cache/* pkgs.mk
-
-clean-all clean-world:V: clean
-	rm -rf ${root}
-
-fetchpkgs = ${pkgs:%=%-fetch}
-
-fetch-all fetch-world:VQ: ${fetchpkgs}
-
-`{ mkdir -p src }
-`{ mkdir -p .cache }
-
-# generate a pkgs.mk file with the targets for all packages in pkgs/
-`{ stuff/generate-pkgs.sh pkgs > pkgs.mk }
-
-<pkgs.mk
-
-bootable:V:
-	stuff/create-bootable "$root" morpheus-$arch-$version.img
+src = ports
+git = git://git.2f30.org/ports
+mkfile = mkfile
