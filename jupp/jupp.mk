9a0e1e6c55550f80ad10c7ff2d2587bb5888ff0b
Daniel Bainton <dpb@driftaway.org>
2014-04-01 12:43:45 +0100

    Add jupp

---
+++ jupp/jupp.mk
@@ -0,0 +1,19 @@
+TARG = jupp
+
+<$mkbuild/mk.common
+
+INSTALL_BIN = joe
+INSTALL_MAN1 = joe.1
+INSTALL_OTHER1 = charmaps/klingon
+INSTALL_OTHER1_DIR = /etc/joe/charmaps
+INSTALL_OTHER2 = joerc jmacsrc
+INSTALL_OTHER2_DIR = /etc/joe
+INSTALL_OTHER3 = `{ls syntax/*}
+INSTALL_OTHER3_DIR = /etc/joe/syntax
+INSTALL_SYMLINK = joe /bin/jmacs \
+	joe.1 $MANDIR/man1/jmacs.1
+
+jupp:QV:
+	sh configure --prefix="$PREFIX"
+	make -j$nprocs
+	
