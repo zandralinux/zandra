8b3eebe6aa684042e3ed106cec95591077d4a786
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-04-24 22:36:30 +0200

    ports: add curl
    initial version
    
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ curl/curl.mk
@@ -0,0 +1,37 @@
+TARG = curl
+DEPS = zlib openssl
+INSTALL_BIN = src/curl
+INSTALL_MAN1 = \
+	docs/curl.1 \
+	docs/curl-config.1
+
+# TODO: add libcurl deps (libcurl.a), and curl deps (/bin/curl).
+# TODO: --with-ca-path="$prefix"/etc/ssl/certs
+
+<$mkbuild/mk.common
+
+curl:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	CC="$CC -static" ./configure --prefix="" \
+		--bindir="$BINDIR" --sbindir="$BINDIR" --libexecdir="$BINDIR" \
+		--mandir="/share/man" \
+		--disable-shared \
+		--disable-ntlm-wb \
+		--disable-pop3 \
+		--disable-imap \
+		--disable-ldap \
+		--disable-ldaps \
+		--disable-telnet \
+		--disable-smtp \
+		--disable-rtsp \
+		--without-nss \
+		--without-libssh2 \
+		--with-ssl=$openssl_libdir \
+		--with-zlib \
+		--enable-static \
+		--enable-ipv6
+	make -j$nprocs
+	# remake static, see: http://curl.haxx.se/mail/archive-2003-03/0115.html
+	rm -f src/curl
+	make LDFLAGS="-all-static $LDFLAGS"
