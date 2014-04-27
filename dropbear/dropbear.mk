9047543aa5a4c267cd7cee417943ff59f16c411a
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-04-27 13:51:18 +0200

    add dropbear
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ dropbear/dropbear.mk
@@ -0,0 +1,19 @@
+TARG = dropbear
+DEPS = zlib
+INSTALL_BIN = \
+	dropbearkey \
+	dropbearconvert \
+	dbclient \
+	dropbear
+INSTALL_MAN1 = \
+	dropbearkey.1 \
+	dropbearconvert.1
+INSTALL_MAN8 = dropbear.8
+
+<$mkbuild/mk.common
+
+dropbear:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	CC="$CC" ./configure --prefix="$prefix" --mandir="$ROOT/share/man"
+	make -j$nprocs
