7d3cdd9c7e4da2ab7137b27054841ec42f9a9a1c
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-04-27 16:56:35 +0200

    add tinyxlib
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ tinyxlib/depsinc.mk
@@ -0,0 +1,4 @@
+<$tinyxlib_DEPDIR/v.mk
+
+DEPS_CFLAGS = $DEPS_CFLAGS -I$tinyxlib_DEPDIR/tinyxlib/include
+DEPS_LDFLAGS = $DEPS_LDFLAGS -L$tinyxlib_DEPDIR/tinyxlib
---
+++ tinyxlib/tinyxlib.mk
@@ -0,0 +1,32 @@
+TARG = tinyxlib
+
+<$mkbuild/mk.common
+
+tinyxlib:QV:
+	make clean
+	make -j$nprocs CC="$CC" BINDIR="/bin" LIBDIR="$prefix/lib" STATIC=1 \
+		FONT_ENCODINGS_DIRECTORY="$prefix/share/fonts/encodings/encodings.dir"
+	# make symlinks for libs, just link to libX11.a like make install does.
+	for f in \
+		libXdmcp.a \
+		libXinerama.a \
+		libXrender.a \
+		libXcursor.a \
+		libXpm.a \
+		libXmuu.a \
+		libSM.a \
+		libXmu.a \
+		libICE.a \
+		libXss.a \
+		libXv.a \
+		libXfont.a \
+		libXext.a \
+		libXt.a \
+		libXau.a \
+		libXtst.a \
+		libXfixes.a \
+		libXi.a \
+		libXaw.a; do
+		ln -sf libX11.a $f
+	done
+#	make DESTDIR="$libcroot" BINDIR="/bin" LIBDIR="/lib" STATIC=1 install
