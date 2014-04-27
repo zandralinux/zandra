e0afff93b8244032e2baf2c3b5a4dcaf577377fb
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-04-27 17:28:00 +0000

    add rsync
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ rsync/rsync.mk
@@ -0,0 +1,11 @@
+TARG = rsync
+
+<$mkbuild/mk.common-noinst
+
+rsync:QV:
+	CC="$CC" ./configure --prefix="$prefix" --mandir="$ROOT/share/man" \
+	   --disable-locale
+	make -j$nprocs
+
+install:QV:
+	make install DESTDIR="$ROOT"
