c513c89d7b4c493aa64f962ec2321514204fe3aa
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-04-27 16:57:01 +0200

    add tinyxserver
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ tinyxserver/tinyxserver.mk
@@ -0,0 +1,27 @@
+TARG = tinyxserver
+DEPS = zlib tinyxlib
+
+<$mkbuild/mk.common-noinst
+
+tinyxserver:QV:
+	return
+	case "$arch" in
+	x86_64)
+		EXTRA_CFLAGS="-D_XSERVER64=1 $CFLAGS"
+		;;
+	i486)
+		EXTRA_CFLAGS="$CFLAGS"
+		;;
+	esac
+	EXTRA_CFLAGS="$EXTRA_CFLAGS $DEPS_CFLAGS $DEPS_LDFLAGS"
+	# fix typo in Makefile to use proper CFLAGS.
+	cp ../Makefile Makefile
+	make clean
+	make -j$nprocs CC="$CC" EXTRA_CFLAGS="$EXTRA_CFLAGS" \
+		FONTDIR="$prefix/share/fonts"
+
+install:QV:
+	make DESTDIR="$ROOT" BINDIR="/bin" install
+	cd "$ROOT/$prefix/share/fonts/misc"
+	mkfontscale
+	mkfontdir
