5cd2c212e0674e8936ae3147f32c630e8500f4dd
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-04-27 17:19:21 +0200

    ii: use install rule from suckless makefile
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- ii/ii.mk
+++ ii/ii.mk
@@ -1,12 +1,11 @@
 TARG = ii
-INSTALL_BIN = \
-	ii
-INSTALL_MAN1 = \
-	ii.1
 
-<$mkbuild/mk.common
+<$mkbuild/mk.common-noinst
 
 ii:QV:
 	# overwrite default config.mk to allow overriding some variables.
 	cp ../config.mk config.mk
 	CC="${CC} -static" make -j$nprocs PREFIX="$prefix" DESTDIR="$ROOT"
+
+install:QV:
+	make -j$nprocs PREFIX="$prefix" DESTDIR="$ROOT" install
