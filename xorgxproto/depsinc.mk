108ce2bfc6743556cfa994b3260ed14703a05429
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-05-02 17:44:25 +0000

    xproto: fix xorgxproto_libdir for depsinc
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- xorgxproto/depsinc.mk
+++ xorgxproto/depsinc.mk
@@ -1,6 +1,7 @@
 <$xorgxproto_DEPDIR/v.mk
 
-xorgxproto_libdir = ${xorgxproto_DEPDIR}/xproto-$v/lib
+xorgxproto_libdir = ${xorgxproto_DEPDIR}/xproto-$v/lib/lib
 xorgxproto_includedir = ${xorgxproto_DEPDIR}/xproto-$v/lib/include
+
 DEPS_CFLAGS = $DEPS_CFLAGS -I${xorgxproto_includedir}
 DEPS_LDFLAGS = $DEPS_LDFLAGS
