0df06b37fac79c8cf5c47118b6544e1d570a258d
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-05-02 20:07:38 +0000

    add libpng
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ libpng/depsinc.mk
@@ -0,0 +1,6 @@
+<$libpng_DEPDIR/v.mk
+
+libpng_libdir = $libpng_DEPDIR/libpng-$v/lib/lib
+libpng_includedir = $libpng_DEPDIR/libpng-$v/lib/include
+DEPS_CFLAGS = $DEPS_CFLAGS -I${libpng_includedir}
+DEPS_LDFLAGS = $DEPS_LDFLAGS -L${libpng_libdir} -lpng
---
+++ libpng/libpng.mk
@@ -0,0 +1,18 @@
+TARG = libpng
+DEPS = zlib
+
+<$mkbuild/mk.common-noinst
+
+libpng:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export CPPFLAGS="${CFLAGS}"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	CC="${CC} -static" ./configure \
+		--prefix="$prefix" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--enable-static
+	# make and install.
+	make -j$nprocs install DESTDIR=`pwd`/lib
+	# remove .la files for now ?
+	find `pwd`/lib -iname "*.la" -exec rm {} \;
