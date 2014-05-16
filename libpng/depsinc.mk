205721d23ab2a545edbbaf741d86bd1c476b1e62
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-05-16 17:15:58 +0000

    libpng include path fix
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- libpng/depsinc.mk
+++ libpng/depsinc.mk
@@ -1,6 +1,6 @@
 <$libpng_DEPDIR/v.mk
 
 libpng_libdir = $libpng_DEPDIR/libpng-$v/lib/lib
-libpng_includedir = $libpng_DEPDIR/libpng-$v/lib/include
+libpng_includedir = $libpng_DEPDIR/libpng-$v/lib/include/libpng16
 DEPS_CFLAGS = $DEPS_CFLAGS -I${libpng_includedir}
-DEPS_LDFLAGS = $DEPS_LDFLAGS -L${libpng_libdir} -lpng
+DEPS_LDFLAGS = $DEPS_LDFLAGS -L${libpng_libdir} -lpng16
