13824d00d763abcf111c1c7a64b366cee1dd39e4
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-05-17 19:13:26 +0200

    fix cares
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- cares/cares.mk
+++ cares/cares.mk
@@ -4,8 +4,15 @@ TARG = cares
 
 cares:QV:
 	export CPPFLAGS="-D_BSD_SOURCE -D_GNU_SOURCE"
-	CC="${CC} -static" CFLAGS="" ./configure --prefix="$prefix" --mandir="$ROOT/share/man" \
+	CC="${CC} -static" CFLAGS="" ./configure \
+		--build="${TOOLCHAIN_TRIPLET}" \
+		--host="${TOOLCHAIN_TRIPLET}" \
+		--prefix="$prefix" \
+		--mandir="$ROOT/share/man" \
 		--disable-werror \
 		--disable-shared \
 		--enable-static
 	make -j$nprocs
+	make -j$nprocs install DESTDIR=`pwd`/lib
+	# remove .la files for now ?
+	find `pwd`/lib -iname "*.la" -exec rm {} \;
--- cares/depsinc.mk
+++ cares/depsinc.mk
@@ -1,6 +1,7 @@
 <$cares_DEPDIR/v.mk
 
-cares_includedir = $cares_DEPDIR/c-ares-$v
-cares_libdir = $cares_DEPDIR/c-ares-$v/.libs
+cares_includedir = $cares_DEPDIR/c-ares-$v/lib/include
+cares_libdir = $cares_DEPDIR/c-ares-$v/lib/lib
+
 DEPS_CFLAGS = $DEPS_CFLAGS -I${cares_includedir}
 DEPS_LDFLAGS = $DEPS_LDFLAGS -L${cares_libdir} -lcares
