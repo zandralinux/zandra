8ce6afe72a42c41d353fa621951b2274f186b3cf
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-05-19 17:01:47 +0200

    bzip2: make static, improve patch and install
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- bzip2/bzip2.mk
+++ bzip2/bzip2.mk
@@ -1,12 +1,15 @@
 TARG = bzip2
+INSTALL_BIN = bzip2 bzip2recover bzgrep bzmore bzdiff
+INSTALL_MAN1 = `ls -1 *.1`
 
-<$mkbuild/mk.common-noinst
+<$mkbuild/mk.common
 
 bzip2:QV:
 	# borrowed from Crux linux.
-	patch -Np1 -i ../bzip2.patch
+	patch --silent --dry-run -N -p1 < ../bzip2.patch && \
+		patch -Np1 < ../bzip2.patch
 	# make everything except doing tests.
 	# NOTE: don't use DESTDIR, it's not used in the Makefile.
 	make libbz2.a bzip2 bzip2recover -j$nprocs \
-		CC="${CC}" AR="${AR}" RANLIB="${RANLIB}" \
-		PREFIX="$prefix"
+		CC="${CC} -static" LDFLAGS="$LDFLAGS" AR="${AR}" \
+		RANLIB="${RANLIB}" PREFIX="$prefix"
