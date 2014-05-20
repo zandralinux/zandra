1017c22528b39ed0cf6cc5b50b1c0b0dfbf319c9
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-05-20 15:22:36 +0200

    add xorgxkbcomp
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ xorgxkbcomp/xorgxkbcomp.mk
@@ -0,0 +1,20 @@
+TARG = xorgxkbcomp
+DEPS = xorglibxkbfile xorglibx11 xorglibxcb xorglibxau xorgxproto \
+	  xorgkbproto xorglibxdmcp
+
+<$mkbuild/mk.common-noinst
+
+xorgxkbcomp:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	export XKBCOMP_CFLAGS="${CFLAGS}"
+	export XKBCOMP_LIBS="${LDFLAGS}"
+	CC="$CC" ./configure \
+		--prefix="$prefix" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--enable-static
+	make -j$nprocs
+
+install:QV:
+	make -j$nprocs install DESTDIR="$ROOT"
