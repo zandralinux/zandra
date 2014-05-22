5d21ebf52d543d1e87500e45906407e93d28f352
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-05-22 12:13:10 +0200

    add xorgvideofbdev
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ xorgvideofbdev/xorgvideofbdev.mk
@@ -0,0 +1,22 @@
+TARG = xorgvideofbdev
+DEPS = xorgserver xorgprotoall xorglibpixman xorglibpciaccess 
+
+<$mkbuild/mk.common-noinst
+
+xorgvideofbdev:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	export XORG_CFLAGS="${CFLAGS}"
+	export XORG_LIBS="${LDFLAGS}"
+	CC="$CC" ./configure \
+		--prefix="$prefix" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--disable-pciaccess \
+		--enable-static
+	make -j$nprocs
+
+install:QV:
+	make -j$nprocs install DESTDIR="$ROOT"
