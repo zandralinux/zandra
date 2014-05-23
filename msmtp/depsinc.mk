a1183f48da9eea3142e9ae998b4fc91d23889e6a
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-05-23 20:12:43 +0200

    msmtp: whoops, remove depsinc
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- msmtp/depsinc.mk
+++
@@ -1,6 +0,0 @@
-<$bzip2_DEPDIR/v.mk
-
-bzip2_libdir = $bzip2_DEPDIR/bzip2-$v
-bzip2_includedir = $bzip2_DEPDIR/bzip2-$v
-DEPS_CFLAGS = $DEPS_CFLAGS -I${bzip2_includedir}
-DEPS_LDFLAGS = $DEPS_LDFLAGS -L${bzip2_libdir} -lbz2
