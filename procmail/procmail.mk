b22600bba604c22ee82f2fed602cbc7a78af80e7
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-05-26 17:20:53 +0200

    add procmail
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ procmail/procmail.mk
@@ -0,0 +1,18 @@
+TARG = procmail
+
+INSTALL_MAN1 = `{ ls man/*.1 } 
+INSTALL_MAN5 = `{ ls man/*.5 } 
+INSTALL_BIN = \
+	src/formail \
+	src/lockfile \
+	src/mailstat \
+	src/procmail \
+	src/lockfile
+
+<$mkbuild/mk.common
+
+procmail:QV:
+	# fix getline().
+	sed -i 's/getline/get_line/' src/*.[ch]
+	# make
+	make -j$nprocs LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
