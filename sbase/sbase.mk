7a1a3d03d532acc95c35f1c5657cb38642cac39c
sin <sin@2f30.org>
2014-05-29 14:36:04 +0100

    Don't install manpages for cp, tar and grep

--- sbase/sbase.mk
+++ sbase/sbase.mk
@@ -3,7 +3,7 @@ OBJ = ${BIN:%=%.o}
 LIB = util.a
 LOBJ = `{ls util/*.c | sed 's/.$/o/'}
 INSTALL_BIN = `{ls *.c | sed 's/..$//' | grep -v '^cp$\|^tar$\|^grep$'}
-INSTALL_MAN1 = `{ls *.1}
+INSTALL_MAN1 = `{ls *.1 | grep -v '^cp\.1$\|^tar\.1$\|^grep\.1$'}
 NPROC = $nprocs
 
 <$mkbuild/mk.common
@@ -11,4 +11,3 @@ NPROC = $nprocs
 &:n: &.o $LIB
 
 $LIB: $LOBJ
-
