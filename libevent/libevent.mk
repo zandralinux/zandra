8cc6bd8d7ad13ffd3e1058019fa1c9862f3fc6ce
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-02 13:58:49 +0200

    libevent: style
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- libevent/libevent.mk
+++ libevent/libevent.mk
@@ -3,8 +3,10 @@ TARG = libevent
 <$mkbuild/mk.common
 
 libevent:QV:
-	CC="$CC" ./configure --prefix="$PREFIX" --mandir="$ROOT/share/man" \
-		 --disable-shared \
+	CC="$CC" ./configure \
+		--prefix="$PREFIX" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
 		--enable-static
 	make -j$nprocs
 	# copy some header files to include dir for depsinc
