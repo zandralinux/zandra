59d101968aa2d108f007f20541a4e877e1b2c1ee
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-02 14:01:51 +0200

    expat: fix static build for xmlwf bin
    add install rule
    
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- expat/expat.mk
+++ expat/expat.mk
@@ -3,12 +3,16 @@ TARG = expat
 <$mkbuild/mk.common-noinst
 
 expat:QV:
-	# TODO: $ROOT/bin/xmlwf is not staticly linked.
 	./configure --prefix="$PREFIX" \
 		--mandir="$ROOT/share/man" \
 		--disable-shared \
 		--enable-static
 	# make and install.
-	make -j$nprocs install DESTDIR="`pwd`/lib"
+	make -j$nprocs install \
+		LDFLAGS="-all-static $LDFLAGS" DESTDIR="`pwd`/lib"
 	# remove .la files for now ?
 	find `pwd`/lib -iname "*.la" -exec rm {} \;
+
+install:QV:
+	make -j$nprocs install \
+		LDFLAGS="-all-static $LDFLAGS" DESTDIR="$ROOT"
