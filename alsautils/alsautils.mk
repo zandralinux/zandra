8b91ad7bf985c91553cede85a7db265bcc98659e
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-05 16:21:07 +0200

    add alsautils
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ alsautils/alsautils.mk
@@ -0,0 +1,21 @@
+TARG = alsautils
+DEPS = alsalib ncurses
+
+<$mkbuild/mk.common-noinst
+
+alsautils:QV:
+	#
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	CC="$CC" ./configure \
+		--prefix="$prefix" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--disable-xmlto \
+		--disable-alsatest \
+		--disable-nls \
+		--enable-static
+	make -j$nprocs
+
+install:QV:
+	make -j$nprocs install DESTDIR="$ROOT"
