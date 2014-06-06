266749b847a17f7d0b2eae6a333906bda8de8b22
sin <sin@2f30.org>
2014-06-06 15:23:57 +0100

    It should -ldrm instead of -libdrm

--- xorglibdrm/depsinc.mk
+++ xorglibdrm/depsinc.mk
@@ -3,4 +3,4 @@
 xorglibdrm_libdir = $xorglibdrm_DEPDIR/libdrm-$v/lib/lib
 xorglibdrm_includedir = $xorglibdrm_DEPDIR/libdrm-$v/lib/include
 DEPS_CFLAGS = $DEPS_CFLAGS -I${xorglibdrm_includedir}
-DEPS_LDFLAGS = $DEPS_LDFLAGS -L${xorglibdrm_libdir} -libdrm
+DEPS_LDFLAGS = $DEPS_LDFLAGS -L${xorglibdrm_libdir} -ldrm
