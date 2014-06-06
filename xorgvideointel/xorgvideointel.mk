1bcbf81eeea0a12dc417b186a49c1ab833cad66d
sin <sin@2f30.org>
2014-06-06 11:49:08 +0100

    Add xorgvideointel

---
+++ xorgvideointel/xorgvideointel.mk
@@ -0,0 +1,23 @@
+TARG = xorgvideointel
+DEPS = xorgserver xorgprotoall xorglibpixman
+
+<$mkbuild/mk.common-noinst
+
+xorgvideointel:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	export XORG_CFLAGS="${CFLAGS}"
+	export XORG_LIBS="${LDFLAGS}"
+	CC="$CC" ./configure \
+		--prefix="$PREFIX" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--disable-pciaccess \
+		--enable-static \
+		--enable-xvmc
+	make -j$nprocs
+
+install:QV:
+	make -j$nprocs install DESTDIR="$ROOT"
