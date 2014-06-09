f1ae60391eaea1bfde8b564ad1375633bbd5371a
sin <sin@2f30.org>
2014-06-09 11:50:36 +0100

    Remove login and passwd from shadow - ubase provides them

--- shadow/shadow.mk
+++ shadow/shadow.mk
@@ -1,21 +1,16 @@
-BIN = login useradd userdel groupadd groupdel passwd
+BIN = useradd userdel groupadd groupdel
 LIB = libcommon.a libmisc.a
 libcommon_a_OBJ = `{ls lib/*.c | sed 's/.$/o/' | grep -v 'tcbfuncs.o$'}
 libmisc_a_OBJ = `{ls libmisc/*.c | sed 's/.$/o/' | grep -v 'xgetXXbyYY.o$'}
-login_OBJ = src/login.o src/login_nopam.o
 useradd_OBJ = src/useradd.o
 userdel_OBJ = src/userdel.o
 groupadd_OBJ = src/groupadd.o
 groupdel_OBJ = src/groupdel.o
-passwd_OBJ = src/passwd.o
 LOCAL_BIN_LDFLAGS = -L. -lcommon -lmisc
 
 LOCAL_CFLAGS = -I. -Ilib -Ilibmisc -DHAVE_CONFIG_H
 
-INSTALL_BIN = login useradd userdel groupadd groupdel passwd
-INSTALL_MAN1 = man/man1/login.1 \
-			   man/man1/passwd.1
-INSTALL_MAN5 = man/man5/passwd.5
+INSTALL_BIN = useradd userdel groupadd groupdel
 INSTALL_MAN8 = man/man8/useradd.8 \
 			   man/man8/userdel.8 \
 			   man/man8/groupadd.8 \
