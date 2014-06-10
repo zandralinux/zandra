4fea1a9057004c406c037249d7cb9e8d0fbdf0a4
sin <sin@2f30.org>
2014-06-10 14:49:15 +0100

    Add support for internal filters to libarchive

--- libarchive/libarchive.mk
+++ libarchive/libarchive.mk
@@ -3,6 +3,7 @@
 BIN = tar/tar cpio/cpio
 LIB = libarchive.a libarchive_fe.a
 TARG = tar.1 cpio.1
+DEPS = zlib
 
 libarchive_a_OBJ = `{ls libarchive/*.c | sed 's/.$/o/' | grep -v '_windows.o$' | grep -v 'archive_entry_copy_bhfi.o$'}
 libarchive_fe_a_OBJ = `{ls libarchive_fe/*.c | sed 's/.$/o/'}
@@ -10,8 +11,8 @@ libarchive_fe_a_OBJ = `{ls libarchive_fe/*.c | sed 's/.$/o/'}
 cpio_cpio_OBJ = `{ls cpio/*.c | sed 's/.$/o/' | grep -v '_windows.o$'}
 tar_tar_OBJ = `{ls tar/*.c | sed 's/.$/o/' | grep -v '_windows.o$'}
 
-LOCAL_BIN_LDFLAGS = -L. -larchive -larchive_fe
-LOCAL_CFLAGS = -I. -Ilibarchive -Ilibarchive_fe -DHAVE_CONFIG_H
+LOCAL_BIN_LDFLAGS = -L. -L${zlib_libdir} -lz -larchive -larchive_fe
+LOCAL_CFLAGS = -I. -I${zlib_includedir} -I. -Ilibarchive -Ilibarchive_fe -DHAVE_CONFIG_H
 
 <$mkbuild/mk.default
 
