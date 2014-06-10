0a322668003a16ffea0ed07c048d4fc395e82ff1
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-10 13:41:52 +0200

    improve libpng (sexification)
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- libpng/depsinc.mk
+++ libpng/depsinc.mk
@@ -1,6 +1,10 @@
 <$libpng_DEPDIR/v.mk
 
-libpng_libdir = $libpng_DEPDIR/libpng-$v/lib/lib
-libpng_includedir = $libpng_DEPDIR/libpng-$v/lib/include/libpng16
-DEPS_CFLAGS = $DEPS_CFLAGS -I${libpng_includedir}
-DEPS_LDFLAGS = $DEPS_LDFLAGS -L${libpng_libdir} -lpng16
+libpng_libdir = ${libpng_DEPDIR}/libpng-$v
+libpng_includedir = ${libpng_DEPDIR}/libpng-$v
+
+libpng_CFLAGS = -I${libpng_includedir}
+libpng_LDFLAGS = -L${libpng_libdir} -lpng
+
+DEPS_CFLAGS = $DEPS_CFLAGS ${libpng_CFLAGS}
+DEPS_LDFLAGS = $DEPS_LDFLAGS ${libpng_LDFLAGS}
--- libpng/libpng.mk
+++ libpng/libpng.mk
@@ -1,18 +1,42 @@
-TARG = libpng
+# TODO: build and package binaries? png2pnm, pngfix, libpng-config etc?
+
+LIB = libpng.a
 DEPS = zlib
+INSTALL_MAN3 = `{ls -1 *.3}
+INSTALL_MAN5 = `{ls -1 *.5}
+INSTALL_LIB = libpng.a
+INSTALL_OTHER1 = png.h pngconf.h pnglibconf.h
+INSTALL_OTHER1_DIR = /include
 
-<$mkbuild/mk.common-noinst
+libpng_a_OBJ = \
+	png.o \
+	pngerror.o \
+	pngget.o \
+	pngmem.o \
+	pngpread.o \
+	pngread.o \
+	pngrio.o \
+	pngrtran.o \
+	pngrutil.o \
+   	pngset.o \
+	pngtrans.o \
+	pngwio.o \
+	pngwrite.o \
+	pngwtran.o \
+	pngwutil.o
 
-libpng:QV:
-	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
-	export CPPFLAGS="${CFLAGS}"
-	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
-	CC="${CC} -static" ./configure \
-		--prefix="$PREFIX" \
-		--mandir="$ROOT/share/man" \
-		--disable-shared \
-		--enable-static
-	# make and install.
-	make -j$nprocs install DESTDIR=`pwd`/lib
-	# remove .la files for now ?
-	find `pwd`/lib -iname "*.la" -exec rm {} \;
+NPROC = $nprocs
+
+<$mkbuild/mk.default
+
+$libpng_a_OBJ:V: pnglibconf.h config.h libpngsymlink
+
+# makes sure -lpng16 works.
+libpngsymlink:
+	ln -sf libpng.a libpng16.a
+
+pnglibconf.h:V:
+	cp ../pnglibconf.h pnglibconf.h
+
+config.h:V:
+	cp ../config.h config.h
