39814e270457ebeb5b8db853b38ce8f48fd98897
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-11 17:27:06 +0200

    add mutt
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ mutt/mutt.mk
@@ -0,0 +1,22 @@
+TARG = mutt
+DEPS = openssl ncurses
+
+INSTALL_MAN1 = `{ ls -1 doc/*.1}
+INSTALL_BIN = mutt
+
+<$mkbuild/mk.common
+
+mutt:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	CC="$CC" ./configure \
+		--prefix="$prefix" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--disable-nls \
+		--disable-iconv \
+		--with-ssl \
+		--with-curses \
+		--with-mailpath="/var/spool/mail" \
+		--enable-static
+	make -j$nprocs
