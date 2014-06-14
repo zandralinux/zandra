69c2ad95b0c745a5ff8682f845ef9d7028ee5d66
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-14 21:14:52 +0200

    add xorgxkeyboardconfig
    initial rough version
    
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ xorgxkeyboardconfig/xorgxkeyboardconfig.mk
@@ -0,0 +1,23 @@
+# runtime deps: xorgxkbcomp
+# make deps: intltool, XML::Parser (p5-xml-parser perl module).
+TARG = xorgxkeyboardconfig
+
+<$mkbuild/mk.common-noinst
+
+$TARG:QV:
+	CC="$CC -static" ./configure \
+		--prefix="$PREFIX" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--disable-nls \
+		--disable-runtime-deps \
+		--with-xkb-base="$PREFIX/share/X11/xkb" \
+		--with-xkb-rules-symlink=xorg \
+		--enable-static	
+	for i in po ; do
+		printf 'all:\n\ttrue\ninstall:\n\ttrue\nclean:\n\ttrue\n' > "$i"/Makefile
+	done
+	make -j$nprocs
+
+install:QV:
+	make -j$nprocs install DESTDIR="$ROOT"
