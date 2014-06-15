9187f06c8a96db9f0e518a31e018fa7bc7625be5
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-15 19:08:04 +0200

    add xf86-input-keyboard and xf86-input-mouse (untested)
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ xf86inputkeyboard/xf86inputkeyboard.mk
@@ -0,0 +1,22 @@
+TARG = xf86inputkeyboard
+DEPS = xorgserver xorglibpixman libpng xorglibpciaccess zlib \
+	xorgprotoall
+#	xorgresourceproto xorgxineramaproto xorgrandrproto xorginputproto \
+#	xorgkbproto xorgvideoproto xorgscrnsaverproto xorgxextproto xorgxproto
+
+<$mkbuild/mk.common-noinst
+
+xf86inputkeyboard:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	export XORG_CFLAGS="$CFLAGS"
+	export XORG_LIBS="$LDFLAGS"
+	CC="$CC" ./configure \
+		--prefix="$prefix" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--enable-static
+	make -j$nprocs
+
+install:QV:
+	make -j$nprocs install DESTDIR="$ROOT"
---
+++ xf86inputmouse/xf86inputmouse.mk
@@ -0,0 +1,19 @@
+TARG = xf86inputmouse
+DEPS = xorgserver xorglibpixman libpng xorglibpciaccess zlib xorgprotoall
+
+<$mkbuild/mk.common-noinst
+
+xf86inputmouse:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	export XORG_CFLAGS="$CFLAGS"
+	export XORG_LIBS="$LDFLAGS"
+	CC="$CC" ./configure \
+		--prefix="$prefix" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--enable-static
+	make -j$nprocs
+
+install:QV:
+	make -j$nprocs install DESTDIR="$ROOT"
