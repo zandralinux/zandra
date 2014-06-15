7551a07369c8d879cbc894f31d06fbcf4fca8390
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-15 19:07:20 +0200

    add some fonts packages: xorg fonts type1 *
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ xorgfontstype1-bitstream/xorgfontstype1-bitstream.mk
@@ -0,0 +1,6 @@
+<$mkbuild/mk.common
+
+INSTALL_OTHER1 = \
+	`{ ls *.afm } \
+	`{ ls *.pfb }
+INSTALL_OTHER1_DIR = $PREFIX/share/fonts/X11/Type1
---
+++ xorgfontstype1-xfree86/xorgfontstype1-xfree86.mk
@@ -0,0 +1,5 @@
+<$mkbuild/mk.common
+
+INSTALL_OTHER1 = \
+	`{ ls *.pfa }
+INSTALL_OTHER1_DIR = $PREFIX/share/fonts/X11/Type1
