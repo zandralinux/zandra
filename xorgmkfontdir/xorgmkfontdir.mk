ce8d571ca7a13771493e5aa4afb0fa7cdd316e64
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-15 19:26:28 +0200

    add mkfontscale and mkfontdir
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ xorgmkfontdir/xorgmkfontdir.mk
@@ -0,0 +1,15 @@
+# TODO: don't configure, just copy mkfontdir, it's just a single script file.
+TARG = xorgmkfontdir
+
+<$mkbuild/mk.common-noinst
+
+xorgmkfontdir:QV:
+	CC="$CC" ./configure \
+		--prefix="$prefix" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--enable-static
+	make -j$nprocs
+
+install:QV:
+	make -j$nprocs install DESTDIR="$ROOT"
---
+++ xorgmkfontscale/xorgmkfontscale.mk
@@ -0,0 +1,21 @@
+TARG = xorgmkfontscale
+DEPS = freetype libpng zlib xorgxproto xorglibfontenc
+
+<$mkbuild/mk.common-noinst
+
+xorgmkfontscale:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	export MKFONTSCALE_CFLAGS="$CFLAGS"
+	export MKFONTSCALE_LIBS="$LDFLAGS"
+	export X11_CFLAGS="$CFLAGS"
+	export X11_LIBS="$LDFLAGS"
+	CC="$CC" ./configure \
+		--prefix="$prefix" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--enable-static
+	make -j$nprocs
+
+install:QV:
+	make -j$nprocs install DESTDIR="$ROOT"
