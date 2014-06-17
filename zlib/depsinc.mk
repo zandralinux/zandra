58776e732785c58569d98fc38cc380dc5a3d9509
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-17 21:43:41 +0200

    zlib: set zlib_CFLAGS and LDFLAGS
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- zlib/depsinc.mk
+++ zlib/depsinc.mk
@@ -1,6 +1,10 @@
 <$zlib_DEPDIR/v.mk
 
-zlib_libdir = $zlib_DEPDIR/zlib-$v
-zlib_includedir = $zlib_DEPDIR/zlib-$v
-DEPS_CFLAGS = $DEPS_CFLAGS -I${zlib_includedir}
-DEPS_LDFLAGS = $DEPS_LDFLAGS -L${zlib_libdir} -lz
+zlib_libdir = ${zlib_DEPDIR}/zlib-$v
+zlib_includedir = ${zlib_DEPDIR}/zlib-$v
+
+zlib_CFLAGS = $DEPS_CFLAGS -I${zlib_includedir}
+zlib_LDFLAGS = $DEPS_LDFLAGS -L${zlib_libdir} -lz
+
+DEPS_CFLAGS = ${zlib_CFLAGS}
+DEPS_LDFLAGS = ${zlib_LDFLAGS}
