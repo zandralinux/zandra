bdd898693bf695b18c8dbef1d3d372e17cd4f51f
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-17 21:44:54 +0200

    pkgtools: fix build against latest git
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- pkgtools/pkgtools.mk
+++ pkgtools/pkgtools.mk
@@ -4,10 +4,10 @@ DEPS = libarchive zlib
 <$mkbuild/mk.common-noinst
 
 pkgtools:QV:
-	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS -DVERSION=\\\"$v\\\""
 	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
 	make -j$nprocs CC="${CC} -static" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" \
-		PREFIX="$PREFIX" DESTDIR="$ROOT"
+		PREFIX="$PREFIX"
 
 install:QV:
 	make -j$nprocs install CC="${CC} -static" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" \
