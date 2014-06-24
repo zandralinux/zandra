5b9616d3dc6bfb2dd8efda8f8a76c3422a482dcc
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-24 17:43:17 +0200

    xorglibx11: add install rule for /share/X11/locale/*
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- xorglibx11/xorglibx11.mk
+++ xorglibx11/xorglibx11.mk
@@ -5,6 +5,10 @@ DEPS = xorgxproto xorglibxcb xorgxcbproto xorgkbproto \
 <$mkbuild/mk.common-noinst
 
 xorglibx11:QV:
+	# fix makekeys Makefile to use host cc
+	# build makekeys staticly.
+	cp ../makekeys_Makefile src/util/Makefile.in
+	# set flags.
 	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
 	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
 	# set these to make sure these are used.
@@ -12,18 +16,23 @@ xorglibx11:QV:
 	export BIGFONT_LIBS="-L${xorgxf86bigfontproto_libdir}"
 	export X11_CFLAGS="${CFLAGS}"
 	export X11_LIBS="${LDFLAGS}"
-	#
 	./configure \
 		--prefix="$PREFIX" \
 		--mandir="$ROOT/share/man" \
 		--with-keysymdefdir="${xorgxproto_includedir}/X11" \
 		--disable-shared \
 		--enable-static
-	# fix makekeys Makefile to use host cc
-	# build makekeys staticly.
-	cp ../makekeys_Makefile src/util/Makefile
 	# make and install.
 	export HOSTCC_CFLAGS="-I${xorgxproto_includedir}"
 	make -j$nprocs install DESTDIR="`pwd`/lib"
 	# remove .la files for now ?
 	find `pwd`/lib -iname "*.la" -exec rm {} \;
+
+# /share/X11/locale/* etc
+install:QV:
+	export HOSTCC_CFLAGS="-I${xorgxproto_includedir}"
+	make -j$nprocs install DESTDIR="$ROOT"
+	rm -rf "$ROOT/lib"
+	rm -rf "$ROOT/include"
+	rm -rf "$ROOT/share/doc"
+	rm -rf "$ROOT/share/man"
