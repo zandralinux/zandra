946dfeff45cfc53696b6f7354563d44c76821127
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-25 00:21:32 +0200

    libpng: add pnm2png and png2pnm binaries
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- libpng/libpng.mk
+++ libpng/libpng.mk
@@ -2,6 +2,12 @@
 
 LIB = libpng.a
 DEPS = zlib
+BIN = \
+	contrib/pngminus/pnm2png \
+	contrib/pngminus/png2pnm
+INSTALL_BIN = \
+	contrib/pngminus/pnm2png \
+	contrib/pngminus/png2pnm
 INSTALL_MAN3 = `{ls -1 *.3}
 INSTALL_MAN5 = `{ls -1 *.5}
 INSTALL_LIB = libpng.a
@@ -25,9 +31,25 @@ libpng_a_OBJ = \
 	pngwtran.o \
 	pngwutil.o
 
+LOCAL_CFLAGS = -I.
+LOCAL_LDFLAGS =
+LOCAL_BIN_LDFLAGS =
+
+contrib_pngminus_pnm2png_LDFLAGS = -L. -lpng
+contrib_pngminus_pnm2png_OBJ = \
+   contrib/pngminus/pnm2png.o
+
+contrib_pngminus_png2pnm_LDFLAGS = -L. -lpng
+contrib_pngminus_png2pnm_OBJ = \
+	contrib/pngminus/png2pnm.o
+
 NPROC = $nprocs
 
 <$mkbuild/mk.default
+
+contrib/pngminus/pnm2png: libpng.a
+
+contrib/pngminus/png2pnm: libpng.a
 
 $libpng_a_OBJ:V: pnglibconf.h config.h libpngsymlink
 
