e994265009d74e20a0d1af684a76cc0caacbdbb4
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-06-30 15:11:28 +0200

    add htop
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ htop/htop.mk
@@ -0,0 +1,19 @@
+TARG = htop
+DEPS = ncurses
+
+INSTALL_BIN = htop
+INSTALL_MAN1 = htop.1
+
+<$mkbuild/mk.common
+
+htop:QV:
+	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
+	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
+	CC="${CC} -static" ./configure \
+		--prefix="$PREFIX" \
+		--mandir="$ROOT/share/man" \
+		--disable-shared \
+		--disable-unicode \
+		--enable-cgroup \
+		--enable-oom
+	make -j$nprocs LDFLAGS="-all-static $LDFLAGS"
