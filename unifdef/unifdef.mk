4580a4a40b66f4a11f6877f9d6ec9cfab5110f9e
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-07-21 20:27:21 +0000

    add unifdef
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

---
+++ unifdef/unifdef.mk
@@ -0,0 +1,17 @@
+BIN = unifdef
+OBJ = unifdef.o
+INSTALL_BIN = unifdef
+INSTALL_MAN1 = `{ls *.1}
+NPROC = $nprocs
+
+<$mkbuild/mk.common
+<../v.mk
+
+LOCAL_CFLAGS = 
+#-DVERSION=\"$v\"
+LOCAL_LDFLAGS = 
+unifdef_LDFLAGS =
+LOCAL_BIN_LDFLAGS =
+DEPS_LDFLAGS =
+
+&:n: &.o
