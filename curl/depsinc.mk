cfb06e7b332f7dff68bfc9ff7729f787a549951a
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-07-21 21:36:39 +0000

    curl: use *_CFLAGS and *_LDFLAGS
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- curl/depsinc.mk
+++ curl/depsinc.mk
@@ -2,5 +2,9 @@
 
 curl_includedir = $curl_DEPDIR/curl-$v/lib/include
 curl_libdir = $curl_DEPDIR/curl-$v/lib/lib
-DEPS_CFLAGS = $DEPS_CFLAGS -I${curl_includedir} -DCURL_STATICLIB
-DEPS_LDFLAGS = $DEPS_LDFLAGS -L${curl_libdir} -lcurl
+
+curl_CFLAGS = -I${curl_includedir} -DCURL_STATICLIB
+curl_LDFLAGS = -L${curl_libdir} -lcurl
+
+DEPS_CFLAGS = $DEPS_CFLAGS ${curl_CFLAGS}
+DEPS_LDFLAGS = $DEPS_LDFLAGS ${curl_LDFLAGS}
