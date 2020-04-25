3d9a75f0fa6ff82714485e4ace9534f64bdd5f2a
darklands <darklands@zandra.xyz>
2020-04-24 22:30:16 -0400

    Fix libressl build

--- libressl/libressl.mk
+++ libressl/libressl.mk
@@ -3,9 +3,6 @@ TARG = libressl
 <$mkbuild/mk.common-noinst
 
 libressl:QV:
-	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
-	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
-	#
 	CC="$CC -static" ./configure \
 		--build="${TOOLCHAIN_TRIPLET}" \
 		--host="${HOST_TOOLCHAIN_TRIPLET}" \
@@ -14,7 +11,9 @@ libressl:QV:
 		--disable-shared \
 		--enable-static
 	# Set path to libssl.a and libcrypto.a for apps/.
-	export LDFLAGS="-all-static $LDFLAGS -L`pwd`/crypto -L`pwd`/ssl -lcrypto -lssl"
+	#export LDFLAGS="-all-static $LDFLAGS -L`pwd`/crypto -L`pwd`/ssl -lcrypto -lssl"
+	mv libtool libtool.old
+	sed -e 's/ -shared / -Wl,-O1,--as-needed -static\0/g' libtool.old > libtool
 	make -j$nprocs V="1" LDFLAGS="$LDFLAGS"
 	# install lib for use as a dependency.
 	make -j$nprocs install DESTDIR="`pwd`/lib" V="1"
