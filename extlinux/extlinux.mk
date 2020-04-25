42b062d921f8e6eedeed52e836c92ecf036dc81e
darklands <darklands@zandra.xyz>
2020-04-24 23:27:31 -0400

    Bump extlinux

--- extlinux/extlinux.mk
+++ extlinux/extlinux.mk
@@ -1,17 +1,18 @@
-BIN = extlinux
+BIN = ../extlinux
 OBJ = bootsect_bin.o ldlinux_bin.o ldlinuxc32_bin.o \
-	  ../libinstaller/syslxcom.o \
-	  ../libinstaller/syslxopt.o \
-	  ../libinstaller/syslxmod.o \
-	  ../libinstaller/setadv.o \
-	  ../libinstaller/advio.o \
-	  ../extlinux/main.o \
-	  ../extlinux/mountinfo.o
+	  libinstaller/syslxcom.o \
+	  libinstaller/fs.o \
+	  libinstaller/syslxopt.o \
+	  libinstaller/syslxmod.o \
+	  libinstaller/setadv.o \
+	  libinstaller/advio.o \
+	  extlinux/main.o \
+	  extlinux/mountinfo.o
 
-INSTALL_BIN = extlinux
-INSTALL_OTHER1 = ../../extlinux.conf
+INSTALL_BIN = ../extlinux
+INSTALL_OTHER1 = ../extlinux.conf
 INSTALL_OTHER1_DIR = /boot
-LOCAL_CFLAGS = -I../libinstaller -I../extlinux -I../bios -D_FILE_OFFSET_BITS=64
+LOCAL_CFLAGS = -Ilibinstaller -Iextlinux -Ibios -D_FILE_OFFSET_BITS=64
 DEPS = e2fsprogs
 
 <$mkbuild/mk.common
@@ -20,14 +21,13 @@ CPPFLAGS =
 
 bin2c:Q:
 	echo "CC $target"
-	$HOSTCC -static -o $target ../../bin2c.c
+	$HOSTCC -static -o $target ../bin2c.c
 
 bootsect_bin.c:Q: bin2c
-	./bin2c ../bios/core/ldlinux.bss $target syslinux_bootsect
+	./bin2c bios/core/ldlinux.bss $target syslinux_bootsect
 ldlinux_bin.c:Q: bin2c
-	./bin2c ../bios/core/ldlinux.sys $target syslinux_ldlinux 512
+	./bin2c bios/core/ldlinux.sys $target syslinux_ldlinux 512
 ldlinuxc32_bin.c:Q: bin2c
-	./bin2c ../bios/com32/elflink/ldlinux/ldlinux.c32 $target syslinux_ldlinuxc32
+	./bin2c bios/com32/elflink/ldlinux/ldlinux.c32 $target syslinux_ldlinuxc32
 
-extlinux: $OBJ
-
+../extlinux: $OBJ
