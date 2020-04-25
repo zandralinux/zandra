cb8bc6a4a79d65137a2b755961a795648a9ffca7
darklands <darklands@zandra.xyz>
2020-04-25 02:04:26 -0400

    Fix compilation for sbase/ubase

---
+++ sbase/config.mk
@@ -0,0 +1,12 @@
+# sbase version
+VERSION = 0.0
+
+# paths
+PREFIX = /
+MANPREFIX = $(PREFIX)/share/man
+
+# for NetBSD add -D_NETBSD_SOURCE
+# -lrt might be needed on some systems
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_FILE_OFFSET_BITS=64
+CFLAGS   = -std=c99 -Wall -pedantic
+LDFLAGS  = -s -static
--- sbase/sbase.mk
+++ sbase/sbase.mk
@@ -3,7 +3,8 @@ TARG = `ls *.c | sed 's/\.c//'`
 <$mkbuild/mk.common-noinst
 
 $TARG:QV:
-	CC="${CC} -static" make -j$nprocs PREFIX="$PREFIX" DESTDIR="$ROOT"
+	cp -f ../config.mk config.mk
+	make -j$nprocs DESTDIR="$ROOT"
 
 install:QV:
-	make -j$nprocs PREFIX="$PREFIX" DESTDIR="$ROOT" install
+	make -j$nprocs DESTDIR="$ROOT" install
