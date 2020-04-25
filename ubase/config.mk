b6a643f159363e3c3283ee6672be39d428b32e6d
darklands <darklands@zandra.xyz>
2020-04-25 16:41:11 -0400

    Fix config.mk

--- ubase/config.mk
+++ ubase/config.mk
@@ -1,11 +1,2 @@
 # ubase version
 VERSION = 0.1
-
-# paths
-PREFIX = /
-MANPREFIX = $(PREFIX)/share/man
-
-CPPFLAGS = -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
-CFLAGS   = -std=c99 -Wall -Wextra
-LDLIBS   = -lcrypt
-LDFLAGS  = -s -static
