8aa603d280bc882a40329c253580c10de72427cb
darklands <darklands@zandra.xyz>
2020-04-26 15:22:03 -0400

    Bump abduco to 0.6

--- abduco/abduco.mk
+++ abduco/abduco.mk
@@ -2,7 +2,7 @@ BIN = abduco
 OBJ = abduco.o
 INSTALL_BIN = abduco
 INSTALL_MAN1 = abduco.1
-LOCAL_CFLAGS = -std=c99 -DVERSION=\"0.1\"
+LOCAL_CFLAGS = -std=c99 -DVERSION=\"0.6\"
 
 <$mkbuild/mk.default
 
